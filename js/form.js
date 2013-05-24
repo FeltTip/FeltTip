0185f7847527c3b1fd74d08392a8cd738828c1ac
Julia Vyushkova <j.vyushkova@gmail.com>
2013-05-25 03:34:27 +0400

    first version of form

---
+++ js/form.js
@@ -0,0 +1,18 @@
+function resource_type(obj)
+{
+    if (obj.value=="book") {
+        articleName = document.getElementById("articleName");
+        articleName.style.visibility = "visible";
+        articleName.style.display = "block";
+    }
+	else if (obj.value=="article") {
+        articleName = document.getElementById("articleName");
+        articleName.style.visibility = "invisible";
+        articleName.style.display = "none";
+    }
+	else if (obj.value=="internet") {
+        articleName = document.getElementById("articleName");
+        articleName.style.visibility = "invisible";
+        articleName.style.display = "none";
+    }	
+}
