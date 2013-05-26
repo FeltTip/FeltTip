bc00328dbdcfdf116146a3498ea76adb3f617d66
ogursan <ogursan@yandex.ru>
2013-05-26 11:12:23 +0400

    Sending quotes to file

--- js/background.js
+++ js/background.js
@@ -2,12 +2,15 @@ $(document).ready(function(){
     chrome.contextMenus.create({
         id: 'FeltTip',
         title: 'Send to FeltTip...',
-        contexts: ['selection']
+        contexts: ['selection'],
+        onclick: function(data){
+            console.log(data);
+            chrome.extension.getBackgroundPage().document.getElementById('quote').innerHTML = data.selectionText;
+            chrome.extension.getBackgroundPage().document.getElementById('source').innerHTML = data.pageUrl;
+            chrome.tabs.create({
+                active: true,
+                url: "form.html"
+            });
+        }
     });
-
-    for (var file in localStorage) {
-        chrome.contextMenus.create({
-            title: 'send'
-        });
-    }
 });
--- js/form.js
+++ js/form.js
@@ -1,21 +1,56 @@
-
 document.onready = function(){
-	this.getElementById('resource_type').onchange = function(obj) {
-	if (obj.value=="book") {
-        articleName = document.getElementById("articleName");
-        articleName.style.visibility = "visible";
-        articleName.style.display = "block";
-    }
-	else if (obj.value=="article") {
-        articleName = document.getElementById("articleName");
-        articleName.style.visibility = "invisible";
-        articleName.style.display = "none";
-    }
-	else if (obj.value=="internet") {
-        articleName = document.getElementById("articleName");
-        articleName.style.visibility = "invisible";
-        articleName.style.display = "none";
-    }	
-	}
-}
+    var files = [];
 
+    for (var file in localStorage) {
+        $('#file').append('<option>' + file + '</option>');
+    }
+
+    var quote = chrome.extension.getBackgroundPage().document.getElementById('quote').innerHTML;
+    var source = chrome.extension.getBackgroundPage().document.getElementById('source').innerHTML;
+    $('#quote').html(quote);
+    var shortLink = source;
+    if (source.length > 60) {
+        shortLink = shortLink.substr(0, 59);
+    }
+    $('#source').html('<a href="' + source + '" target="_blank" >' + shortLink + '...</a>');
+
+    $('#send').on('click', function(){
+        var fileName = $('#file').val();
+        if (fileName == '') {
+            alert('Please, select file to send the quote');
+            return false;
+        }
+
+        var tab = $('.tab-pane.active ');
+        var author = $(tab).find('input[name="author"]').val();
+        var title = $(tab).find('input[name="title"]').val();
+        var year = $(tab).find('input[name="year"]').val();
+        var publisher = $(tab).find('input[name="publisher"]').val();
+        var quote = $('#quote').html();
+        var source = $('#source').html();
+
+        var file = JSON.parse(localStorage[fileName]);
+        var key = file.quotes.length;
+        file.quotes[key] = {
+            quote: quote,
+            source: source,
+            author: author,
+            title: title,
+            year: year,
+            publisher: publisher
+        };
+
+        localStorage[fileName] = JSON.stringify(file);
+
+        chrome.tabs.getCurrent(function(tab){
+            var notification = webkitNotifications.createNotification(
+                'i/48x48.png',
+                'Success',
+                'Quote sended to "' + fileName + '"'
+            );
+            notification.show();
+            chrome.tabs.update(tab.id, {url: 'edit.html?file=' + fileName});
+
+        });
+    });
+}
